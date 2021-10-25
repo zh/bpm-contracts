2f5c0d144c7d04c1035e5b7b2b3a446d6369c3bb
Stoyan Zhekov <zh@zhware.net>
2021-10-25 12:09:24 +0900

    No frontend, so no ownership transfer

--- deploy/00_deploy_smart_lock.js
+++ deploy/00_deploy_smart_lock.js
@@ -1,7 +1,5 @@
 const { ethers } = require("hardhat");
 
-const OWNER_ADDR = "0x81585790aA977b64e0c452DB84FC69eaCE951d4F"; // adjust from frontend
-
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -9,12 +7,5 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     from: deployer,
     log: true,
   });
-
-  console.log("\n Transfer ownership to the frontend address...\n");
-  const contract = await ethers.getContractAt(
-    "SmartLock",
-    lockContract.address
-  );
-  await contract.transferOwnership(OWNER_ADDR);
 };
 module.exports.tags = ["SmartLock"];
